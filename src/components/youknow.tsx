cdfd759326586776902cc4e1fe27519815989c34
Marilia Cimali da Costa Camara Caldas <marilia@ibm.com>
2022-10-07 04:26:48 -0300

    chore: final changes to youknow component

--- src/components/youknow.tsx
+++ src/components/youknow.tsx
@@ -13,7 +13,7 @@ export const YouKnow = () => {
   return (
     <section className="youknow--section flex flex-col items-center pt-6 md:pt-0 pb-16 px-8 md:px-0">
       <div className="flex flex-col justify-center items-center">
-        <h3 className="w-full max-w-[700px] font-bauer text-center text-xl md:text-4xl my-10 text-grad-2 px-8">
+        <h3 className="w-full max-w-[700px] font-bauer text-center text-xl md:text-4xl mt-10 mb-1 md:my-10 text-grad-2 px-6">
           Você sabe tudo que vem dentro da caixinha de Koleston
         </h3>
         <div className="flex flex-col md:grid md:grid-cols-3 gap-6 w-full max-w-[900px] px-12 md:px-0 hidden">
@@ -50,30 +50,31 @@ export const YouKnow = () => {
             </div>
           ))}
         </div>
-        <div className="flex flex-col gap-6 w-full flex md:hidden">
+        <div className="block md:hidden">
           {youKnowItems.map((item: Item) => (
-            <div
-              key={item.id}
-              className="flex flex-row md:flex-col items-center"
-            >
-              <div className="youknow md:flex md:items-center md:justify-center">
+            <div key={item.id} className="flex items-center gap-4">
+              <div className="w-[30%] inline-block">
                 <img
                   className="youknow--image"
                   src={item.image}
                   alt={item.title}
                 />
               </div>
-              <div className="text-left md:text-center">
+              <div className="w-[70%] inline-block text-left md:text-center">
                 {item.displayTag && (
                   <span>
-                    <img src="/assets/images/new.png" alt="NOVO" />
+                    <img
+                      className="max-w-[50px]"
+                      src="/assets/images/new.png"
+                      alt="NOVO"
+                    />
                   </span>
                 )}
-                <h4 className="avenir-black uppercase text-grad">
+                <h4 className="avenir-black uppercase text-sm text-grad">
                   {item.title}
                 </h4>
                 <div
-                  className="avenir-black"
+                  className="avenir-black text-sm"
                   dangerouslySetInnerHTML={{ __html: item.description }}
                 ></div>
               </div>
